27adfee4929c7f072917a49661eb7ab58e39902b
stuartburrill <stuart.burrill@btinternet.com>
2018-03-18 17:35:54 +0000

    My first test commit

--- helloworld2.py
+++ helloworld2.py
@@ -1,2 +1,12 @@
 # This is a test
+import boto3
+
 print('Hello-World')
+
+#Create a list
+mylist = ("Stuart", "Amanda", "Harriet", "Mia")
+
+i = 0
+while i < len(mylist):
+    print('Family Member' $i mylist[i])
+    i = i + 1
